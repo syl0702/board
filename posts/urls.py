7491c03d8b10a21c6d3f44a737102520d2565d69
syl0702 <navyblue794@gmail.com>
2023-08-23 14:51:44 +0900

    Update 기능 구현

--- posts/urls.py
+++ posts/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('create/', views.create, name='create'),
 
     # Delete
-    path('<int:id>/delete/', views.delete, name='delete')
+    path('<int:id>/delete/', views.delete, name='delete'),
+
+    # Update
+    path('<int:id>/edit/', views.edit, name='edit'),
+    path('<int:id>/update/', views.update, name='update'),
 ]
